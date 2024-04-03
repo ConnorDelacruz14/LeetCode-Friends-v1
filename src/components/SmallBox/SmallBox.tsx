26e31e29ee879835f92c5008ddd7011a8a2c888d
Connor Delacruz <ConnorD1432@gmail.com>
2024-04-02 21:42:37 -0700

    Awards

--- src/components/SmallBox/SmallBox.tsx
+++ src/components/SmallBox/SmallBox.tsx
@@ -2,10 +2,12 @@ import './SmallBox.css'
 
 type Props = {
     title: string;
+    height?: string;
 }
 
-export const SmallBox: React.FC<Props> = ({title}) => {
-    return <div className="small-card">{title}</div>;
+export const SmallBox: React.FC<Props> = ({title, height}) => {
+    const className = 'small-card ' + height;
+    return <div className={className}>{title}</div>;
 }
 
 export default SmallBox;
--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -20,22 +20,22 @@ export const Popup = () => {
           <SmallBox title="Sign Out"></SmallBox>
         </div>
         <div className="card-column column-2">
-          <SmallBox title="Username"></SmallBox>
-          <SmallBox title="Session Code:"></SmallBox>
+          <SmallBox title="Username" height="h-20"></SmallBox>
+          <SmallBox title="Session Code:" height="h-20"></SmallBox>
           <ItemBox title="Friends"></ItemBox>
-          <SmallBox title=""></SmallBox>
-          <SmallBox title="Join Session:"></SmallBox>
+          <SmallBox title="" height="h-20"></SmallBox>
+          <SmallBox title="Join Session:" height="h-20"></SmallBox>
           <div className='awards-container'>
-              <Award src="img/award.png" alt="award" desc="Honorable - Place 1st in 3 seperate sessions"></Award>
-              <Award src="img/competition.png" alt="competition" desc="Competitor - Get top 3 in a single session"></Award>
-              <Award src="img/fire.png" alt="fire" desc="On Fire - Solve problems for 14 consecutive days "></Award>
-              <Award src="img/friends.png" alt="friends" desc="Friendly - Add 3 Friends"></Award>
-              <Award src="img/gladiator.png" alt="gladiator" desc="Weekend Warrior - Solve a problem on the weekend"></Award>
-              <Award src="img/level-up.png" alt="level up" desc="Experienced - Get to level 100"></Award>
-              <Award src="img/thumbs-up.png" alt="thumbs up" desc="Likeable - Someone liked your profile"></Award>
-              <Award src="img/mountain.png" alt="mountain" desc="First Steps - Solve a LeetCode problem"></Award>
-              <Award src="img/rewards.png" alt="reward" desc="Champion - Place first in a single session"></Award>
-              <Award src="img/wizard.png" alt="wizard" desc="Wizard - Solve 500 Problems"></Award>
+              <Award active="not-active" src="img/award.png" alt="award" desc="Honorable - Place 1st in 3 seperate sessions"></Award>
+              <Award active="not-active" src="img/competition.png" alt="competition" desc="Competitor - Get top 3 in a single session"></Award>
+              <Award active="not-active" src="img/fire.png" alt="fire" desc="On Fire - Solve problems for 14 consecutive days "></Award>
+              <Award active="not-active" src="img/friends.png" alt="friends" desc="Friendly - Add 3 Friends"></Award>
+              <Award active="not-active" src="img/gladiator.png" alt="gladiator" desc="Weekend Warrior - Solve a problem on the weekend"></Award>
+              <Award active="not-active" src="img/level-up.png" alt="level up" desc="Experienced - Get to level 100"></Award>
+              <Award active="not-active" src="img/thumbs-up.png" alt="thumbs up" desc="Likeable - Someone liked your profile"></Award>
+              <Award active="not-active" src="img/mountain.png" alt="mountain" desc="First Steps - Solve a LeetCode problem"></Award>
+              <Award active="not-active" src="img/rewards.png" alt="reward" desc="Champion - Place first in a single session"></Award>
+              <Award active="not-active" src="img/wizard.png" alt="wizard" desc="Wizard - Solve 500 Problems"></Award>
           </div>
         </div>
       </div>
